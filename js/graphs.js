aa70aceb8b2321bbbed9e18ef38dc3971d5828d2
Kaylynn <silentttone@gmail.com>
2016-12-05 16:25:01 -0500

    added graphs and styling

--- js/graphs.js
+++ js/graphs.js
@@ -1,9 +1,77 @@
 //data[0] = benefits, data[1] = leave, data[2] = productivity
 var parsedData = getData();
 
-var chart1 = c3.generate({
+var benefits = c3.generate({
     title: {
       text: 'Company Insurance Covers Mental Health Benefits'
+    },
+    data: {
+        json: parsedData[0],
+        type: 'bar',
+        keys: {
+          x: 'x',
+          value: ['total']
+        }
+    },
+    legend: {
+      position: 'right',
+      show: false
+    },
+    color: {
+      pattern: ['rgb(63, 188, 210)']
+    },
+    axis: {
+      y: {
+        label: {
+          text: "Count",
+          position: "outer-middle"
+        }
+      },
+      x: {
+        type: 'category'
+      }
+    },
+    bindto: "#benefits"
+});
+
+
+var leave = c3.generate({
+    title: {
+      text: 'How Easily Employees Can Take Leave for Mental Illness'
+    },
+    data: {
+        json: parsedData[1],
+        type: 'bar',
+        keys: {
+          x: 'x',
+          value: ['total']
+        }
+    },
+    legend: {
+      position: 'right',
+      show: false
+    },
+    color: {
+      pattern: ['rgb(166, 44, 161)']
+    },
+    axis: {
+      y: {
+        label: {
+          text: "Count",
+          position: "outer-middle"
+        }
+      },
+      x: {
+        type: 'category'
+      }
+    },
+    bindto: "#leave"
+});
+
+
+var productivity = c3.generate({
+    title: {
+      text: 'Does Mental Illness Affect Your Productivity'
     },
     data: {
         json: parsedData[2],
@@ -14,7 +82,11 @@ var chart1 = c3.generate({
         }
     },
     legend: {
-      position: 'right'
+      position: 'right',
+      show: false
+    },
+    color: {
+      pattern: ['rgb(215, 55, 13)']
     },
     axis: {
       y: {
@@ -24,12 +96,8 @@ var chart1 = c3.generate({
         }
       },
       x: {
-        type: 'category',
-        label: {
-          text: "Category",
-          position: 'outer-center'
-        }
+        type: 'category'
       }
     },
-    bindto: "#chart1"
+    bindto: "#productivity"
 });
